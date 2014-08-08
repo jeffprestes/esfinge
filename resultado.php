1961fb94d5d0bab531f9062ad728789a2aa158e3
jeffprestes <jeffprestes@gmail.com>
2014-08-08 13:03:41 -0300

    Redirecionando o usuario a tela de exibição dos dados depois do login executado com sucesso

--- resultado.php
+++ resultado.php
@@ -3,7 +3,7 @@ session_start();
 
 $usuario = $_SESSION["usuario"];
 
-print_r($usuario);
+var_dump($usuario);
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
